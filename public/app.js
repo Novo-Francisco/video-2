b8a2ec77ff728f7b855f799dd7f40e1755652add
Novo-Francisco <vitormotar@hotmail.com>
2026-05-14 16:43:00 -0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ btnRender.addEventListener("click", async function () {
 setError("");
 setLog("Chamando /generate...");
 try {
-const res = await fetch("/api/generate", {
+const res = await fetch("/generate", {
 method: "POST",
 headers: {
 "Content-Type": "application/json"
